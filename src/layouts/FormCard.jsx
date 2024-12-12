f73fa86e9dff6d174c61b12ac74f64c900095242
jomarth <jomarthemanuel@gmail.com>
2024-12-12 15:54:39 -0600

    logo update

--- src/layouts/FormCard.jsx
+++ src/layouts/FormCard.jsx
@@ -7,7 +7,7 @@ function FormCard({children, title}) {
                 <div className={'flex flex-col pb-[5em] justify-center items-center'}>
                     <h1 className='text-2xl font-bold text-center pb-2 text-blue-800'>{title}</h1>
                     <img className={'w-1/5 h-1/5'}
-                        src="../../public/images/logo.jpeg"
+                        src="https://res.cloudinary.com/dmmlxlio3/image/upload/v1734040288/kkh7z3uvzfpota73pniw.jpg"
                          alt="No se encontro la imagen"/>
                 </div>
                 {children}
